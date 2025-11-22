f5095ee9bff9c3dc086641b85f342cdf8c3e4746
DominicDev <s0l0estudi4r@gmail.com>
2025-11-22 16:34:50 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LE CASTLE VANIA';
+export let nameSongs = [
+	"BLOOD CODE", /*1*/
+	"LED SPIRALS", /*2*/
+	"WAKE", /*3*/
+	"NOBODY GETS OUT ALIVE", /*4*/
+	"INCARNATION", /*5*/
+	"DISINTEGRATION", /*6*/
+	"CARRY ON FEATURING MARIANA BELL", /*7*/
+	"DRIVING AWAY", /*8*/
+	"THE LIGHTS", /*9*/
+	"PRAY FOR MY ENEMIES", /*10*/
+	"D.O.A", /*11*/
+	"NUMBA ONE", /*12*/
+	"I WANT YOU", /*13*/
+	"AFTER IMAGE", /*14*/
+	"PROPHICATION", /*15*/
+	"YOU KNOW MY NAME", /*16*/
+	"BLOOD FLOWS", /*17*/
+	"THE JOURNEY", /*18*/
+	"KEEP BREATHING", /*19*/
+	"BAD COMPANY", /*20*/
+]
